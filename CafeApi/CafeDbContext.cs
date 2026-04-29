398f7605245ae150fef379c08236e6a1c8013d9c
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2026-04-29 13:43:21 +0200

    initialize and configure solution

---
+++ CafeApi/CafeDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CafeApi;
+
+public class CafeDbContext : DbContext
+{
+    public CafeDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
---
+++ CafeTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace CafeTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
