1e68ca8ba20cd48679d2f158802d0314d9ba9276
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2026-04-30 01:36:42 +0200

    add MenuItemRepository

---
+++ CafeApi/Interfaces/IMenuItemRepository.cs
@@ -0,0 +1,9 @@
+﻿using CafeApi.Enums;
+using CafeApi.Models;
+
+namespace CafeApi.Interfaces;
+
+public interface IMenuItemRepository : IRepository<MenuItem>
+{
+    Task<IEnumerable<MenuItem>> GetItemsByCategoryAsync(ItemCategory category);
+}
---
+++ CafeApi/Repositories/MenuRepository.cs
@@ -0,0 +1,21 @@
+﻿using CafeApi.Data;
+using CafeApi.Enums;
+using CafeApi.Interfaces;
+using CafeApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CafeApi.Repositories;
+
+public class MenuRepository : Repository<MenuItem>, IMenuItemRepository
+{
+    public MenuRepository(CafeDbContext db) : base(db)
+    {
+    }
+
+    public async Task<IEnumerable<MenuItem>> GetItemsByCategoryAsync(ItemCategory category)
+    {
+        return await Db.Set<MenuItem>()
+            .Where(menuItem => menuItem.Category == category)
+            .ToListAsync();
+    }
+}
