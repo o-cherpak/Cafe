86f71e12b8cd3b2d3ac052887aa8d2cf43dd2581
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2026-04-30 12:02:23 +0200

    add OrderRepository

---
+++ CafeApi/Interfaces/IOrderRepository.cs
@@ -0,0 +1,10 @@
+﻿using CafeApi.Enums;
+using CafeApi.Models;
+
+namespace CafeApi.Interfaces;
+
+public interface IOrderRepository : IRepository<Order>
+{
+    Task<IEnumerable<Order>> GetOrderByStatusAsync(OrderStatus status);
+    Task<IEnumerable<Order>> GetOrderByCustomerIdAsync(int customerId);
+}
---
+++ CafeApi/Repositories/OrderRepository.cs
@@ -0,0 +1,26 @@
+﻿using CafeApi.Data;
+using CafeApi.Enums;
+using CafeApi.Interfaces;
+using CafeApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CafeApi.Repositories;
+
+public class OrderRepository : Repository<Order>, IOrderRepository
+{
+    public OrderRepository(CafeDbContext db) : base(db)
+    {
+    }
+
+    public async Task<IEnumerable<Order>> GetOrderByStatusAsync(OrderStatus status)
+    {
+        return await Db.Orders.Where(order => order.Status == status)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Order>> GetOrderByCustomerIdAsync(int customerId)
+    {
+        return await Db.Orders.Where(order => order.CustomerId == customerId)
+            .ToListAsync();
+    }
+}
