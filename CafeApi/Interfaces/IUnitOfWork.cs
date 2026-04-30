00f64bb138407877d19451cb3766817e18b42c6d
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2026-04-30 12:39:19 +0200

    add UnitOfWork

---
+++ CafeApi/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace CafeApi.Interfaces;
+
+public interface IUnitOfWork : IDisposable
+{
+    IMenuItemRepository MenuItems { get; }
+    ICustomerRepository Customers { get; }
+    IOrderRepository Orders { get; }
+    Task SaveChangesAsync();
+}
---
+++ CafeApi/Repositories/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using CafeApi.Data;
+using CafeApi.Interfaces;
+
+namespace CafeApi.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly CafeDbContext _db;
+
+    public IMenuItemRepository MenuItems { get; }
+    public ICustomerRepository Customers { get; }
+    public IOrderRepository Orders { get; }
+
+    public UnitOfWork(CafeDbContext db)
+    {
+        _db = db;
+        MenuItems = new MenuRepository(_db);
+        Customers = new CustomerRepository(_db);
+        Orders = new OrderRepository(_db);
+    }
+
+    public void Dispose()
+    {
+        _db.Dispose();
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _db.SaveChangesAsync();
+    }
+}
