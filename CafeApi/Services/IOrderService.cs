acdf12ba35837926bbd912b9aae34a1632f0f0e8
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2026-05-02 11:39:19 +0200

    add CreateAsync realisation in OrderService

--- CafeApi/Services/IOrderService.cs
+++ CafeApi/Services/IOrderService.cs
@@ -7,7 +7,5 @@ namespace CafeApi.Services;
 public interface IOrderService
 {
     OrderResponseDto ToDto(Order order);
-    Task<IEnumerable<OrderResponseDto>> GetAllAsync(int? customerId);
-    Task<OrderResponseDto?> GetByIdAsync(int id);
     Task<OrderResponseDto> CreateAsync(CreateOrderDto dto);
 }
--- CafeApi/Services/OrderService.cs
+++ CafeApi/Services/OrderService.cs
@@ -1,11 +1,19 @@
 ﻿using CafeApi.DTOs;
 using CafeApi.Enums;
+using CafeApi.Interfaces;
 using CafeApi.Models;
 
 namespace CafeApi.Services;
 
 public class OrderService : IOrderService
 {
+    private readonly IUnitOfWork _uow;
+
+    public OrderService(IUnitOfWork uow)
+    {
+        _uow = uow;
+    }
+
     public OrderResponseDto ToDto(Order order)
     {
         var dto = new OrderResponseDto(
@@ -25,18 +33,58 @@ public class OrderService : IOrderService
         return dto;
     }
 
-    public Task<IEnumerable<OrderResponseDto>> GetAllAsync(int? customerId)
+    public async Task<OrderResponseDto> CreateAsync(CreateOrderDto dto)
     {
-        throw new NotImplementedException();
-    }
+        //Customer
+        var customer = await _uow.Customers.GetByIdAsync(dto.CustomerId);
 
-    public Task<OrderResponseDto?> GetByIdAsync(int id)
-    {
-        throw new NotImplementedException();
-    }
+        if (customer is null)
+            throw new KeyNotFoundException("Customer is not found");
 
-    public Task<OrderResponseDto> CreateAsync(CreateOrderDto dto)
-    {
-        throw new NotImplementedException();
+        //Order
+        var order = new Order
+        {
+            CustomerId = dto.CustomerId,
+            Status = OrderStatus.Pending,
+            CreatedAt = DateTime.UtcNow,
+            Items = []
+        };
+
+        //MenuItem
+        foreach (var itemDto in dto.Items)
+        {
+            var menuItem = await _uow.MenuItems.GetByIdAsync(itemDto.MenuItemId);
+
+            if (menuItem is null)
+                throw new KeyNotFoundException($"Item {itemDto.MenuItemId} not found");
+
+            if (!menuItem.IsAvailable)
+                throw new InvalidOperationException($"{menuItem.Name} is unavailable");
+
+            order.Items.Add(
+                new OrderItem
+                {
+                    MenuItemId = menuItem.Id,
+                    Quantity = itemDto.Quantity,
+                    UnitPrice = menuItem.Price
+                }
+            );
+        }
+
+        //Bonus
+        var total = order.Items.Sum(i => i.UnitPrice * i.Quantity);
+        customer.BonusPoints += (int)(total * 10);
+
+        //Save
+        await _uow.Orders.AddAsync(order);
+        _uow.Customers.Update(customer);
+        await _uow.SaveChangesAsync();
+
+        var saved = await _uow.Orders.GetByIdAsync(order.Id);
+
+        if (saved is null)
+            throw new KeyNotFoundException("Order not found");
+
+        return ToDto(saved);
     }
 }
