71560afaf6524e968a30c9210a73136beb3f1848
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2026-05-12 11:28:34 +0200

    add CustomerPromotionController

---
+++ CafeApi/Controllers/CustomerPromotionController.cs
@@ -0,0 +1,85 @@
+﻿using CafeApi.DTOs;
+using CafeApi.Services.CustomerPromotionService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CafeApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CustomerPromotionController : ControllerBase
+{
+    private readonly ICustomerPromotionService _service;
+
+    public CustomerPromotionController(ICustomerPromotionService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<CustomerPromotionDto>>> GetAll()
+    {
+        var result = await _service.GetAll();
+
+        return Ok(result);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<CustomerPromotionDto>> GetById(int id)
+    {
+        var result = await _service.GetById(id);
+
+        return Ok(result);
+    }
+
+    [HttpGet("by-customer")]
+    public async Task<ActionResult<IEnumerable<CustomerPromotionDto>>>
+        GetByCustomerIdAsync([FromQuery] int customerId)
+    {
+        var result =
+            await _service.GetByCustomerIdAsync(customerId);
+
+        return Ok(result);
+    }
+
+    [HttpGet("by-customer-promotion")]
+    public async Task<ActionResult<CustomerPromotionDto>>
+        GetByCustomerAndPromotionAsync(
+            [FromQuery] int customerId, [FromQuery] int promotionId
+        )
+    {
+        var result =
+            await _service.GetByCustomerAndPromotionAsync(customerId, promotionId);
+
+        return Ok(result);
+    }
+
+    [HttpGet("by-order")]
+    public async Task<ActionResult<IEnumerable<CustomerPromotionDto>>>
+        GetByOrderIdAsync([FromQuery] int orderId)
+    {
+        var result = await _service.GetByOrderAsync(orderId);
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<CustomerPromotionDto>>
+        BuyPromotion(BuyPromotionDto buyPromotionDto)
+    {
+        var dto = await _service.BuyPromotion(buyPromotionDto);
+
+        return CreatedAtAction(
+            nameof(GetById),
+            new { id = dto.Id },
+            dto
+        );
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> Delete(int id)
+    {
+        await _service.Delete(id);
+
+        return NoContent();
+    }
+}
