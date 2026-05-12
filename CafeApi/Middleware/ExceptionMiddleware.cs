6064ed47cb9cbc8ddf07500676331662ca18e432
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2026-05-12 12:14:04 +0200

    update ExceptionMiddleware by adding new exceptions handler and delete json content type

--- CafeApi/Middleware/ExceptionMiddleware.cs
+++ CafeApi/Middleware/ExceptionMiddleware.cs
@@ -21,25 +21,26 @@ public class ExceptionMiddleware
         catch (NotFoundException ex)
         {
             context.Response.StatusCode = 404;
-            context.Response.ContentType = "application/json";
             await context.Response.WriteAsJsonAsync(new { error = ex.Message });
         }
         catch (ConflictException ex)
         {
             context.Response.StatusCode = 409;
-            context.Response.ContentType = "application/json";
             await context.Response.WriteAsJsonAsync(new { error = ex.Message });
         }
-        catch (InvalidOperationException ex)
+        catch (InsufficientBonusException ex)
         {
             context.Response.StatusCode = 400;
-            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsJsonAsync(new { error = ex.Message });
+        }
+        catch (PromotionNotActiveException ex)
+        {
+            context.Response.StatusCode = 400;
             await context.Response.WriteAsJsonAsync(new { error = ex.Message });
         }
         catch (Exception)
         {
             context.Response.StatusCode = 500;
-            context.Response.ContentType = "application/json";
             await context.Response.WriteAsJsonAsync(new { error = "Something went wrong" });
         }
     }
