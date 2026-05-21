f0d5854fafb89036065153c6c40e55f72675cb9f
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2026-05-21 22:02:26 +0200

    remove IsRequired constraint from User.CustomerId

---
+++ CafeApi/Migrations/20260521195638_RemoveIsRequiredFromUser.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CafeApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveIsRequiredFromUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CustomerId",
+                table: "Users",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CustomerId",
+                table: "Users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+    }
+}
