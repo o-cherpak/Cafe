5896d49bca6e8b75b736de9766e8de89e1b9219a
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2026-05-23 11:50:26 +0200

    add unit tests for CreateCustomerValidator to validate Name and Email fields

--- CafeTests/UnitTests/CustomerServiceTests.cs
+++ CafeTests/UnitTests/CustomerServiceTests.cs
@@ -4,6 +4,7 @@ using CafeApi.Exceptions.NotFoundExceptions;
 using CafeApi.Models;
 using CafeApi.Repositories;
 using CafeApi.Services.CustomerService;
+using CafeApi.Validators.CustomerValidators;
 using FluentAssertions;
 
 namespace CafeTests.UnitTests;
@@ -93,6 +94,41 @@ public class CustomerServiceTests
 
         _db.Customers.Should().HaveCount(2);
         _db.Customers.Should().ContainSingle(c => c.Email == dto.Email && c.Name == dto.Name);
+    }
+    
+    [Fact]
+    public async Task Validator_CreateCustomer_EmptyName_ReturnsError()
+    {
+        var validator = new CreateCustomerValidator();
+        var dto = new CreateCustomerDto("", "john@gmail.com");
+
+        var result = await validator.ValidateAsync(dto);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(e => e.PropertyName == "Name");
+    }
+
+    [Fact]
+    public async Task Validator_CreateInvalidEmailTest()
+    {
+        var validator = new CreateCustomerValidator();
+        var dto = new CreateCustomerDto("John", "email");
+
+        var result = await validator.ValidateAsync(dto);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(e => e.PropertyName == "Email");
+    }
+
+    [Fact]
+    public async Task Validator_CreateNoErrorsTest()
+    {
+        var validator = new CreateCustomerValidator();
+        var dto = new CreateCustomerDto("Alex", "alex@gmail.com");
+
+        var result = await validator.ValidateAsync(dto);
+
+        result.IsValid.Should().BeTrue();
     }
 
     [Fact]
