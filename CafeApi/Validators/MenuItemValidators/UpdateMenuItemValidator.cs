cd438164de97eeb3da95690e4562494804da822c
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2026-05-23 11:41:12 +0200

    add UpdateMenuItemValidator to validate UpdateMenuItemDto fields

---
+++ CafeApi/Validators/MenuItemValidators/UpdateMenuItemValidator.cs
@@ -0,0 +1,19 @@
+﻿using CafeApi.DTOs;
+using FluentValidation;
+
+namespace CafeApi.Validators;
+
+public class UpdateMenuItemValidator : AbstractValidator<UpdateMenuItemDto>
+{
+    public UpdateMenuItemValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name cannot be empty")
+            .MaximumLength(140).WithMessage("Name max 140 characters")
+            .When(x => x.Name is not null);
+
+        RuleFor(x => x.Price)
+            .GreaterThan(0).WithMessage("Price must be greater than 0")
+            .When(x => x.Price is not null);
+    }
+}
