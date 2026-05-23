f5c221a632112f3d7faf225083ccbe1390e4a36f
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2026-05-23 23:20:40 +0200

    add RegisterValidator and LoginValidator to validate RegisterDto and LoginDto fields

---
+++ CafeApi/Validators/UserValidators/LoginValidator.cs
@@ -0,0 +1,17 @@
+﻿using CafeApi.DTOs;
+using FluentValidation;
+
+namespace CafeApi.Validators.UserValidators;
+
+public class LoginValidator : AbstractValidator<LoginDto>
+{
+    public LoginValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Invalid email.");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required.");
+    }
+}
