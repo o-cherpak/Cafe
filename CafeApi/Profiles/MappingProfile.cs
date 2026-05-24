aecda5cffcc2912cf7c66a90b3d3d7ed05d189e9
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2026-05-24 17:05:17 +0200

    add MappingProfile to configure object mappings between models and DTOs using AutoMapper

---
+++ CafeApi/Profiles/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CafeApi.DTOs;
+using CafeApi.Models;
+
+namespace CafeApi.Profiles;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Customer, CustomerDto>();
+        
+        CreateMap<MenuItem, MenuItemDto>();
+        CreateMap<Order, OrderResponseDto>();
+        CreateMap<OrderItem, OrderItemDto>(); 
+        
+        CreateMap<CustomerPromotion, CustomerPromotionDto>();
+        CreateMap<Promotion, PromotionDto>();
+    }
+}
