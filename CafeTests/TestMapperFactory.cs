020aed268c982eb767d6cfebf4b5f53e8e4ac1d4
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2026-05-25 16:33:44 +0200

    add TestMapperFactory to create IMapper instances for unit tests

---
+++ CafeTests/TestMapperFactory.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CafeApi.Profiles;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace CafeTests;
+
+public static class TestMapperFactory
+{
+    public static IMapper Create()
+    {
+        var expression = new MapperConfigurationExpression();
+        expression.AddProfile<MappingProfile>();
+
+        var config = new MapperConfiguration(expression, NullLoggerFactory.Instance);
+
+        return config.CreateMapper();
+    }
+}
