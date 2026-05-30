13701bd505fb24d5559f5c6f1073dfbfc554b03e
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2026-05-30 19:41:14 +0200

    reorder imports in main.tsx for better consistency in CafeUI

--- CafeUI/src/main.tsx
+++ CafeUI/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import {router} from "./router/router.tsx";
+import {router} from "./router/router.tsx";   
 import {RouterProvider} from "react-router-dom";
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
