00ac7e85471e0ca3fd439a664940447cc1d0e7c7
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2026-06-02 15:44:53 +0200

    add MenuItemConfiguration

---
+++ CafeApi/ModelConfigurations/MenuItemConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using CafeApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CafeApi.ModelConfigurations;
+
+public class MenuItemConfiguration : IEntityTypeConfiguration<MenuItem>
+{
+    public void Configure(EntityTypeBuilder<MenuItem> builder)
+    {
+        builder
+            .Property(m => m.Price)
+            .HasColumnType("decimal(10,2)");
+
+        builder
+            .Property(m => m.Name)
+            .HasMaxLength(140);
+
+        builder
+            .Property(m => m.Description)
+            .HasMaxLength(400);
+
+        builder
+            .Property(m => m.Category)
+            .HasConversion<string>()
+            .HasMaxLength(100);
+    }
+}
