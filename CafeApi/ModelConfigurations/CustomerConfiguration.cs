6104d2d85a0f6a65e4a2f3d16baf1f9fae695b73
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2026-06-02 15:35:04 +0200

    add CustomerConfiguration

---
+++ CafeApi/ModelConfigurations/CustomerConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using CafeApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CafeApi.ModelConfigurations;
+
+public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        builder
+            .HasIndex(c => c.Email)
+            .IsUnique();
+
+        builder
+            .Property(c => c.Name)
+            .IsRequired()
+            .HasMaxLength(140);
+
+        builder
+            .Property(c => c.Email)
+            .IsRequired()
+            .HasMaxLength(200);
+    }
+}
