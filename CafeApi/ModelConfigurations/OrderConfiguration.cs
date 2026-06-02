33884897e1f3c5dd71fb9f1857d4e8643c8e1e13
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2026-06-02 15:49:51 +0200

    add OrderConfiguration

---
+++ CafeApi/ModelConfigurations/OrderConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using CafeApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CafeApi.ModelConfigurations;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder
+            .HasOne(o => o.Customer)
+            .WithMany(c => c.Orders)
+            .HasForeignKey(o => o.CustomerId);
+
+        builder
+            .Property(o => o.Status)
+            .HasConversion<string>();
+    }
+}
