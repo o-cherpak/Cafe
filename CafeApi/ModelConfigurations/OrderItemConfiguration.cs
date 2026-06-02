763d22ebd4eb6d030782714992f061c7b7a5774b
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2026-06-02 15:50:02 +0200

    add OrderItemConfiguration

---
+++ CafeApi/ModelConfigurations/OrderItemConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using CafeApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CafeApi.ModelConfigurations;
+
+public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        builder
+            .HasOne(oi => oi.MenuItem)
+            .WithMany()
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder
+            .Property(oi => oi.UnitPrice)
+            .HasColumnType("decimal(10,2)");
+    }
+}
