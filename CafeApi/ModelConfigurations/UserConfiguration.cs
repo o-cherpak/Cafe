162429c35bfc1aeaa9f59ba7c0e97c90cb0d56a2
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2026-06-02 16:02:37 +0200

    add UserConfiguration

---
+++ CafeApi/ModelConfigurations/UserConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using CafeApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CafeApi.ModelConfigurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder
+            .HasOne(u => u.Customer)
+            .WithOne()
+            .HasForeignKey<User>(u => u.CustomerId)
+            .OnDelete(DeleteBehavior.SetNull);
+
+        builder
+            .HasIndex(u => u.Email)
+            .IsUnique();
+
+        builder
+            .Property(u => u.Email)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder
+            .Property(u => u.Role)
+            .HasConversion<string>();
+    }
+}
